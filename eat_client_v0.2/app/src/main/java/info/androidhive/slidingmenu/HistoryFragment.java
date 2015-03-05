abd00c5647f38315b097d15ade2b234698f27f91
JohnL <John@johns-air-6.wireless.dartmouth.edu>
2015-03-05 04:55:08 -0500

    added to history

--- eat_client_v0.2/app/src/main/java/info/androidhive/slidingmenu/HistoryFragment.java
+++ eat_client_v0.2/app/src/main/java/info/androidhive/slidingmenu/HistoryFragment.java
@@ -8,17 +8,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.graphics.Color;
 
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import lecho.lib.hellocharts.model.SubcolumnValue;
+import lecho.lib.hellocharts.model.Column;
+import lecho.lib.hellocharts.model.ColumnChartData;
 import info.androidhive.slidingmenu.api.ServerConnect;
 import info.androidhive.slidingmenu.interfaces.ApiAsyncResponse;
+import lecho.lib.hellocharts.listener.ColumnChartOnValueSelectListener;
 import lecho.lib.hellocharts.listener.LineChartOnValueSelectListener;
 import lecho.lib.hellocharts.listener.PieChartOnValueSelectListener;
 import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
 import lecho.lib.hellocharts.model.Line;
 import lecho.lib.hellocharts.model.LineChartData;
 import lecho.lib.hellocharts.model.PieChartData;
@@ -27,6 +33,7 @@ import lecho.lib.hellocharts.model.SliceValue;
 import lecho.lib.hellocharts.model.ValueShape;
 import lecho.lib.hellocharts.model.Viewport;
 import lecho.lib.hellocharts.util.ChartUtils;
+import lecho.lib.hellocharts.view.ColumnChartView;
 import lecho.lib.hellocharts.view.LineChartView;
 import lecho.lib.hellocharts.view.PieChartView;
 
@@ -36,7 +43,8 @@ public class HistoryFragment extends Fragment implements ApiAsyncResponse {
     private int numberOfLines = 4;
     private int maxNumberOfLines = 4;
     private int numberOfPoints = 12;
-
+    private ColumnChartView chart;
+    private ColumnChartData data;
     private boolean hasAxes = true;
     private boolean hasAxesNames = true;
     private boolean hasLines = true;
@@ -46,6 +54,7 @@ public class HistoryFragment extends Fragment implements ApiAsyncResponse {
     private boolean hasLabels = false;
     private boolean isCubic = false;
     private boolean hasLabelForSelected = false;
+    public static final int COLOR_VIOLET = Color.parseColor("#AA66CC");
 
     float[][] randomNumbersTab = new float[maxNumberOfLines][numberOfPoints];
     private PieChartView piechart;
@@ -63,6 +72,14 @@ public class HistoryFragment extends Fragment implements ApiAsyncResponse {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_history, container, false);
+
+        chart = (ColumnChartView) rootView.findViewById(R.id.chart);
+        chart.setOnValueTouchListener(new ValueTouchListener());
+
+        generateDefaultData();
+
+
+
         linechart = (LineChartView) rootView.findViewById(R.id.linechart);
         linechart.setOnValueTouchListener(new LineChartValueTouchListener());
         // Generate some random values.
@@ -92,6 +109,52 @@ public class HistoryFragment extends Fragment implements ApiAsyncResponse {
         s.execute("GET" ,"fetch_history_params" , "");
     }
 
+    private void generateDefaultData() {
+        int numSubcolumns = 1;
+        int numColumns = 12;
+        String[] months = {"Jan", "Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+        int[] completionValues = {70,74,85,89,95,100,92,84,79,83,92,97};
+        // Column can have many subcolumns, here by default I use 1 subcolumn in each of 8 columns.
+        List<Column> columns = new ArrayList<Column>();
+        List<SubcolumnValue> values;
+        for (int i = 0; i < numColumns; ++i) {
+
+            values = new ArrayList<SubcolumnValue>();
+            for (int j = 0; j < numSubcolumns; ++j) {
+                values.add(new SubcolumnValue(completionValues[i], COLOR_VIOLET));
+            }
+
+            Column column = new Column(values);
+            column.setHasLabels(hasLabels);
+            column.setHasLabelsOnlyForSelected(hasLabelForSelected);
+            columns.add(column);
+        }
+
+
+        data = new ColumnChartData(columns);
+
+        if (hasAxes) {
+            List<AxisValue> axisValues = new ArrayList<AxisValue>();
+            for (int i = 0; i < numColumns; ++i) {
+                axisValues.add(new AxisValue(i, months[i].toCharArray()));
+            }
+            Axis axisX = new Axis(axisValues);
+            Axis axisY = new Axis().setHasLines(false);
+            if (hasAxesNames) {
+                axisX.setName("Month");
+                axisY.setName("Completion");
+            }
+            data.setAxisXBottom(axisX);
+            data.setAxisYLeft(axisY);
+        } else {
+            data.setAxisXBottom(null);
+            data.setAxisYLeft(null);
+        }
+
+        chart.setColumnChartData(data);
+
+    }
+
     private void generateValuesforLine() {
         for (int i = 0; i < maxNumberOfLines; ++i) {
             for (int j = 0; j < numberOfPoints; ++j) {
@@ -109,6 +172,21 @@ public class HistoryFragment extends Fragment implements ApiAsyncResponse {
         v.right = numberOfPoints -1;
         linechart.setMaximumViewport(v);
         linechart.setCurrentViewport(v);
+    }
+
+    private class ValueTouchListener implements ColumnChartOnValueSelectListener {
+
+        @Override
+        public void onValueSelected(int columnIndex, int subcolumnIndex, SubcolumnValue value) {
+            Toast.makeText(getActivity(), "Selected: " + value, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onValueDeselected() {
+            // TODO Auto-generated method stub
+
+        }
+
     }
 
     private void generateDataforLine() {
