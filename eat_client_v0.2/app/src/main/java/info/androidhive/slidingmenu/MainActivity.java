84de48e1b726acc9bffb294091900b0cf4c5eb6b
shirleyma <mazhenshirley@gmail.com>
2015-03-05 11:36:43 -0500

    change login view

--- eat_client_v0.2/app/src/main/java/info/androidhive/slidingmenu/MainActivity.java
+++ eat_client_v0.2/app/src/main/java/info/androidhive/slidingmenu/MainActivity.java
@@ -98,7 +98,12 @@ public class MainActivity extends Activity {
 			public void onDrawerClosed(View view) {
 //				getActionBar().setTitle(mTitle);
                 super.onDrawerClosed(view);
+                getActionBar().show();
+                View decorView = getWindow().getDecorView();
+                int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
+                decorView.setSystemUiVisibility(uiOptions);
                 shouldGoInvisible = false;
+
 				// calling onPrepareOptionsMenu() to show action bar icons
 				invalidateOptionsMenu();
 			}
@@ -106,7 +111,11 @@ public class MainActivity extends Activity {
 			public void onDrawerOpened(View view) {
 //				getActionBar().setTitle(mDrawerTitle);
                 super.onDrawerOpened(view);
+                View decorView = getWindow().getDecorView();
+                int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+                decorView.setSystemUiVisibility(uiOptions);
                 shouldGoInvisible = true;
+                getActionBar().hide();
 				// calling onPrepareOptionsMenu() to hide action bar icons
 				invalidateOptionsMenu();
 			}
